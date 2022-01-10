c4d8b6dccf77cb8d8396b6068f26aebcfb82cda5
BuiVinh <buiqvinh147@gmail.com>
2022-01-10 21:50:22 +0700

    UI

---
+++ client/travail-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.scss';
+
+// Custom components
+import AppBar from './components/AppBar/AppBar';
+import BoardBar from './components/BoardBar/BoardBar';
+import BoardContent from './components/BoardContent/BoardContent';
+
+
+function App() {
+  return (
+    <div className="travail-app">
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+  
+     
+    </div>
+  );
+}
+
+export default App;
